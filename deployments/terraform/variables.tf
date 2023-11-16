e544550e42b3a74c3a198a2a07bc53fdc14eca1d
Lee Hyohwak <crewe1746@naver.com>
2023-11-16 16:22:11 +0900

    Create aws ecrs

---
+++ deployments/terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "common_tags" {
+  type        = map(string)
+  description = "Common tags to apply to all resources"
+  default = {
+    "Project" = "Hero Ticket"
+  }
+}
+
+variable "ecr_repos" {
+  type = map(object({
+    name                 = string
+    image_tag_mutability = string
+    scan_on_push         = bool
+    name_tag             = string
+  }))
+  description = "ECR repositories to create"
+  default = {
+    "hero-ticket-server" = {
+      name                 = "hero-ticket-server"
+      image_tag_mutability = "MUTABLE"
+      scan_on_push         = true
+      name_tag             = "hero-ticket-server"
+    }
+    "hero-ticket-swagger" = {
+      name                 = "hero-ticket-swagger"
+      image_tag_mutability = "MUTABLE"
+      scan_on_push         = true
+      name_tag             = "hero-ticket-swagger"
+    }
+  }
+}
