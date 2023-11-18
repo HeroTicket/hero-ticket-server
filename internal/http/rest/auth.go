7a3f6cd3879a3f1d26ee814d5d61a5b73bd449d6
Lee Hyohwak <crewe1746@naver.com>
2023-11-18 15:33:40 +0900

    Implement notice controller

--- internal/http/rest/auth.go
+++ internal/http/rest/auth.go
@@ -11,7 +11,7 @@ func AccessTokenRequired(jwtSvc jwt.Service) func(next http.Handler) http.Handle
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			cookie, err := r.Cookie("access_token")
 			if err != nil {
-				ErrorJSON(w, "access token not found in cookie")
+				ErrorJSON(w, "access token not found in cookie", http.StatusUnauthorized)
 				return
 			}
 
@@ -19,7 +19,7 @@ func AccessTokenRequired(jwtSvc jwt.Service) func(next http.Handler) http.Handle
 
 			jwtUser, err := jwtSvc.VerifyToken(accessToken, jwt.TokenRoleAccess)
 			if err != nil {
-				ErrorJSON(w, "failed to validate access token")
+				ErrorJSON(w, "failed to validate access token", http.StatusUnauthorized)
 				return
 			}
 
--- internal/http/rest/rest.go
+++ internal/http/rest/rest.go
@@ -1,6 +1,13 @@
 package rest
 
+import "github.com/heroticket/internal/notice"
+
 type CommonResponse struct {
 	Status  int    `json:"status"`
 	Message string `json:"message"`
+}
+
+type NoticesResponse struct {
+	Notices    []*notice.Notice   `json:"notices"`
+	Pagination *notice.Pagination `json:"pagination"`
 }
