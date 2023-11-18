d50fd416a730a637a7c8d65c5fef7be02e85f035
Lee Hyohwak <crewe1746@naver.com>
2023-11-18 23:40:50 +0900

    Add notice service implementation

--- internal/notice/service.go
+++ internal/notice/service.go
@@ -9,3 +9,31 @@ type Service interface {
 	UpdateNotice(ctx context.Context, params *NoticeUpdateParams) error
 	DeleteNotice(ctx context.Context, id string) error
 }
+
+type noticeService struct {
+	repo Repository
+}
+
+func New(repo Repository) Service {
+	return &noticeService{repo: repo}
+}
+
+func (svc *noticeService) CreateNotice(ctx context.Context, n *Notice) (*Notice, error) {
+	return svc.repo.CreateNotice(ctx, n)
+}
+
+func (svc *noticeService) DeleteNotice(ctx context.Context, id string) error {
+	return svc.repo.DeleteNotice(ctx, id)
+}
+
+func (svc *noticeService) GetNotice(ctx context.Context, id string) (*Notice, error) {
+	return svc.repo.GetNotice(ctx, id)
+}
+
+func (svc *noticeService) GetNotices(ctx context.Context, page int64, limit int64) ([]*Notice, *Pagination, error) {
+	return svc.repo.GetNotices(ctx, page, limit)
+}
+
+func (svc *noticeService) UpdateNotice(ctx context.Context, params *NoticeUpdateParams) error {
+	return svc.repo.UpdateNotice(ctx, params)
+}
