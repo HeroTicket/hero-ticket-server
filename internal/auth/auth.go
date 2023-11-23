014b6c17047702468350a63d7db2bd4aabccf6ff
Lee Hyohwak <crewe1746@naver.com>
2023-11-23 17:58:26 +0900

    Add auth service

---
+++ internal/auth/auth.go
@@ -0,0 +1,18 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/iden3/iden3comm/v2/protocol"
+)
+
+var DefaultTimeout = 10 * time.Minute
+
+type AuthorizationRequestParams struct {
+	ID          string
+	Reason      string
+	Message     string
+	Sender      string
+	CallbackUrl string
+	Scope       []protocol.ZeroKnowledgeProofRequest
+}
