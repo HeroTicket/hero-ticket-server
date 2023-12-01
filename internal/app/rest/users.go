586798e99427754f68cc74a8efc8c3e95d5fcf69
Lee Hyohwak <crewe1746@naver.com>
2023-12-01 21:34:26 +0900

    Update refresh

--- internal/app/rest/users.go
+++ internal/app/rest/users.go
@@ -233,8 +233,6 @@ type RefreshTokenRequest struct {
 //	@Failure		500			{object}	CommonResponse
 //	@Router			/v1/users/refresh [post]
 func (c *UserCtrl) refresh(w http.ResponseWriter, r *http.Request) {
-	r.Body = http.MaxBytesReader(w, r.Body, 1024*1024) // 1MB
-
 	// 1. read token from request body
 	var req RefreshTokenRequest
 
@@ -247,9 +245,12 @@ func (c *UserCtrl) refresh(w http.ResponseWriter, r *http.Request) {
 
 	defer r.Body.Close()
 
+	fmt.Println(req)
+
 	// 2. validate token
 	jwtUser, err := c.jwt.VerifyToken(req.RefreshToken, jwt.TokenRoleRefresh)
 	if err != nil {
+		logger.Error("invalid token", "error", err)
 		ErrorJSON(w, "invalid token", http.StatusBadRequest)
 		return
 	}
