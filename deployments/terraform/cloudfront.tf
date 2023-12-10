eeab6e63ccd168bb980bc6e326b06685ed560e25
Lee Hyohwak <crewe1746@naver.com>
2023-12-10 19:28:37 +0900

    Fix cloudfront 403, 404 error by redirecting to index.html

--- deployments/terraform/cloudfront.tf
+++ deployments/terraform/cloudfront.tf
@@ -90,6 +90,18 @@ resource "aws_cloudfront_distribution" "hero_ticket_client_distribution" {
     }
   }
 
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
